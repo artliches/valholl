3298d68d78c9ca2b7188ead1fd66cecfe8ebf391
Leo Velez <leonardo.velez@kaplan.edu>
2026-01-26 14:27:42 -0500

    v0.2 - identity section feature complete

--- src/app/names/names.ts
+++ src/app/names/names.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FIRST_NAME, LAST_NAME, NICKNAME } from '../assets/valholl.constants';
+import { BELIEFS, FIRST_NAME, HABITS, LAST_NAME, NICKNAME, PAST, QUIRKS } from '../assets/valholl.constants';
 import { RandomNumber } from '../services/random-number';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-names',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './names.html',
   styleUrl: './names.scss',
 })
@@ -31,27 +32,67 @@ export class Names implements OnInit {
     currValue: -1,
   };
 
-  ngOnInit(): void {
-      this.firstNameArray = this.randomNumberService.shuffle(FIRST_NAME);
-      this.lastNameArray = this.randomNumberService.shuffle(LAST_NAME);
-      this.nickNameArray = this.randomNumberService.shuffle(NICKNAME);
+  pastArray: string[] =[];
+  pastObj = {
+    descrip: '',
+    currValue: -1,
+  };
 
-      this.firstNameObj = this.reroll(this.firstNameArray, this.firstNameObj);
-      this.lastNameObj = this.reroll(this.lastNameArray, this.lastNameObj);
-      this.nickNameObj = this.reroll(this.nickNameArray, this.nickNameObj);
+  quirkArray: string[] =[];
+  quirkObj = {
+    descrip: '',
+    currValue: -1,
+  };
+
+  habitsArray: string[] =[];
+  habitsObj = {
+    descrip: '',
+    currValue: -1,
+  };
+
+  beliefArray: string[] =[];
+  beliefObj = {
+    descrip: '',
+    currValue: -1,
+  };
+
+  ngOnInit(): void {
+      this.shuffleArrays();
+      this.rerollAllObjects();
   }
 
-  reroll(nameArray: string[], nameObj: {descrip: string, currValue: number}): {descrip: string, currValue: number} {
-    const isEndOfArray = nameArray.length === nameObj.currValue + 1;
+  rerollAllObjects() {
+    this.firstNameObj = this.reroll(this.firstNameArray, this.firstNameObj);
+    this.lastNameObj = this.reroll(this.lastNameArray, this.lastNameObj);
+    this.nickNameObj = this.reroll(this.nickNameArray, this.nickNameObj);
+
+    this.pastObj = this.reroll(this.pastArray, this.pastObj);
+    this.quirkObj = this.reroll(this.quirkArray, this.quirkObj);
+    this.beliefObj = this.reroll(this.beliefArray, this.beliefObj);
+    this.habitsObj = this.reroll(this.habitsArray, this.habitsObj);
+  }
+
+  private shuffleArrays() {
+    this.firstNameArray = this.randomNumberService.shuffle(FIRST_NAME);
+    this.lastNameArray = this.randomNumberService.shuffle(LAST_NAME);
+    this.nickNameArray = this.randomNumberService.shuffle(NICKNAME);
+    this.pastArray = this.randomNumberService.shuffle(PAST);
+    this.quirkArray = this.randomNumberService.shuffle(QUIRKS);
+    this.beliefArray = this.randomNumberService.shuffle(BELIEFS);
+    this.habitsArray = this.randomNumberService.shuffle(HABITS);
+  }
+
+  reroll(constArray: string[], displayObj: {descrip: string, currValue: number}): {descrip: string, currValue: number} {
+    const isEndOfArray = constArray.length === displayObj.currValue + 1;
 
     if(isEndOfArray) {
-      nameArray = this.randomNumberService.shuffle(nameArray);
+      constArray = this.randomNumberService.shuffle(constArray);
     }
 
-    const newValue = isEndOfArray ? 0 : nameObj.currValue + 1;
+    const newValue = isEndOfArray ? 0 : displayObj.currValue + 1;
 
     return {
-      descrip: nameArray[newValue],
+      descrip: constArray[newValue],
       currValue: newValue,
     };
   }
